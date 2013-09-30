fbfda9423b357b198567ee9a8d0906d6d42c1ca7
lermert <laura.ermert@erdw.ethz.ch>
2013-09-30 11:07:09 +0200

    Plot correlations by distance from one selected station.

--- TOOLS/plot_dist.py
+++ TOOLS/plot_dist.py
@@ -8,26 +8,31 @@ from TOOLS.read_xml import read_xml
 import matplotlib.pyplot as plt
 import numpy as np
 
-def plot_dist(indir, xmldir, station, corrtype, stacktype, verbose=False, savefig=False):
+def plot_dist(indir, xmldir, station, corrtype, ps_nu, verbose=False, savefig=False):
     """
     A script to plot cross-correlation traces sorted by interstation distance.
     indir: string: path to directory containing results of stack.py
     xmldir: A directory containing the station xml files for all stations (to get the distances). These files must be in xml format and must be called network.station.sta_info.xml
     station: The 'reference': All other stations are plotted with respect to the station selected here and distance is distance to this station
     corrtype: string: cc or pcc
-    stacktype: string: ls or pws; show the linear or the phase weighted stack (I need to implement first that actually the type is specified on the output file.
+    ps_nu: integer; power to which the weighting by instantaneous phase stack is raised. ps=0 means linear stack.
     """
+    #Initialize the plot
+    fig=plt.figure(figsize=(10, 16), dpi=100)
+    fig.hold()
+    plt.subplot(111)
+    plt.rc('axes', color_cycle=['b', 'g', 'k'])
     
     #Find the files containing a correlation of the 'reference' station
     sta=re.compile(station)
-    lin=re.compile('lin_stack')
+    ctype=re.compile(corrtype+'_stack')
     
     ls=os.listdir(indir)
     stalist=[]
     
     for filename in ls:
         if sta.search(filename) is not None:
-            if lin.search(filename) is not None:
+            if ctype.search(filename) is not None:
                 stalist.append(filename)
         else:
             continue
@@ -43,10 +48,19 @@ def plot_dist(indir, xmldir, station, corrtype, stacktype, verbose=False, savefi
         
         correlation=read(indir+'/'+file)[0]
         
-        if stacktype=='pws':
-            psfile=lin.sub('phase_stack', file)
-            pstack=read(indir+'/'+psfile)[0]
-            correlation.data=correlation.data*pstack.data
+        if ps_nu==0:
+            stacktype='ls'
+        else:
+            stacktype='pws'+str(ps_nu)
+            fhre=ctype.sub('coherence_stack_real', file)
+            fhim=ctype.sub('coherence_stack_imag', file)
+            
+            phre=read(indir+'/'+fhre)[0].data
+            phim=read(indir+'/'+fhim)[0].data
+            #Calculate the phase stack
+            pstack=np.power(np.sqrt(np.multiply(phre, phre)+np.multiply(phim, phim)), ps_nu)
+            
+            correlation.data=np.multiply(correlation.data, pstack)
             
         taxis=np.linspace(-(len(correlation.data)-1)/2/correlation.stats.sampling_rate,(len(correlation.data)-1)/2/correlation.stats.sampling_rate, len(correlation.data))
         
@@ -62,14 +76,17 @@ def plot_dist(indir, xmldir, station, corrtype, stacktype, verbose=False, savefi
            dist=gps2DistAzimuth(lat1, lon1, lat2, lon2)[0]
            
         plt.plot(taxis, correlation.data/np.max(correlation.data)+dist/10000)
+        plt.annotate(inf[1]+'-'+inf[5], xy=(taxis[0], dist/10000) , xytext=(taxis[0]-150, dist/10000), fontsize=8 )
         
     plt.xlabel('Lag Time (sec)')
     plt.ylabel('Cross-Correlation / Interstation distance')
+    plt.xlim((taxis[0]-200, taxis[-1:]+200))
     frame1=plt.gca()
     frame1.axes.get_yaxis().set_ticks([])
     
+    
     if savefig==True:
-        figname=indir+'/'+station+'_'+stacktype+'.'+corrtype+'.png'
+        figname=indir+'/'+station+'.'+stacktype+'.'+corrtype+'.png'
         plt.savefig(figname, format='png')
         
     plt.show()
@@ -117,3 +134,6 @@ def get_sta_info(indir, filt):
             continue
     os.system('rm temp.txt') 
           
+
+
+
