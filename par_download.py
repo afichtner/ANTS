1c440712f707f0680ae0af5b1982070fd4605172
Laura Lermert <laura.ermert@erdw.ethz.ch>
2014-09-24 12:14:40 +0200

    optional segments for data download

--- par_download.py
+++ par_download.py
@@ -89,7 +89,10 @@ def par_download(xmlinput):
     t2str=UTCDateTime(t2).strftime('%Y.%j.%H.%M.%S')
  
     # data segment length
-    winlen = int(dat['time']['len'])
+    if dat['time']['len']=='':
+        winlen=UTCDateTime(t2)-UTCDateTime(t1)
+    else
+        winlen = int(dat['time']['len'])
     
     # minimum length
     minlen=dat['time']['minlen']
