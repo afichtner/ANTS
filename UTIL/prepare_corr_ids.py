a52073385db8831cbab5efa678986c2a290c2dec
Laura Lermert <laura.ermert@erdw.ethz.ch>
2014-11-06 16:56:55 +0100

    The tutorial case.

--- UTIL/prepare_corr_ids.py
+++ UTIL/prepare_corr_ids.py
@@ -1,7 +1,11 @@
 from glob import glob
 import antconfig as cfg
+import os
 
 def prepare_corr_ids(indir,prepname):
+    
+    if os.path.exists('INPUT/ID_LISTS')==False:
+        os.mkdir('INPUT/ID_LISTS')
     
     outfile='INPUT/ID_LISTS/ids_'+prepname+'_corr.txt'
     fid = open(outfile,'w')
@@ -13,6 +17,8 @@ def prepare_corr_ids(indir,prepname):
     
     ids_un = list()
     
+    print 'I found the following suitable channel IDs for correlation:'
+    
     for id in idlist:
         
         id = id.split('/')[-1]
@@ -23,9 +29,10 @@ def prepare_corr_ids(indir,prepname):
             print id
             fid.write(id+'\n')
             ids_un.append(id)
-            
+           
+    print 'They are saved under:'
+    print outfile
     fid.close()
-    return()
         
         
     
