53134fb62669f035c224763436477578819f30f6
Laura Lermert <laura.ermert@erdw.ethz.ch>
2015-03-02 16:40:23 +0100

    Removed dataprov for download

--- par_download.py
+++ par_download.py
@@ -142,11 +142,8 @@ def par_download(xmlinput):
                 print '\n Attempting to download data from: '+id+'\n'
                 print filename
                 
-                if dat['datprov'] == 'iris':
-                    reqstring=exdir+'/FetchData '+vfetchdata+' -N '+network+ ' -S '+station+' -C '+channel+' -s '+tstart+' -e '+tstep+' -msl '+minlen+' --lat '+lat_min+':'+lat_max+' --lon '+lon_min+':'+lon_max+' -o '+filename
-                elif dat['datprov'] == 'eth':
-                    reqstring=exdir+'/FetchDataArc '+vfetchdata+' -N '+network+ ' -S '+station+' -C '+channel+' -s '+tstart+' -e '+tstep+' --lat '+lat_min+':'+lat_max+' --lon '+lon_min+':'+lon_max+' -o '+filename
-                    
+                reqstring=exdir+'/FetchData '+vfetchdata+' -N '+network+ ' -S '+station+' -C '+channel+' -s '+tstart+' -e '+tstep+' -msl '+minlen+' --lat '+lat_min+':'+lat_max+' --lon '+lon_min+':'+lon_max+' -o '+filename
+                  
                 os.system(reqstring)
             t += winlen
           
